3983f20bdae3c721627bf2a571ec82eeeea20b30
Chris Cankov <cankov.chris@gmail.com>
2017-06-18 12:20:32 -0700

    Clean up file structure

--- lib/components/navheader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NavHeader = () => (
-  <nav>
-    <h1>Sudokulor</h1>
-  </nav>
-);
-
-export default NavHeader;
--- lib/sudokulor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './components/game';
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  let root = document.getElementById('root');
-  ReactDOM.render( <Game /> , root);
-});
